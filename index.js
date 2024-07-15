6f5b328c5cab3e90f41944adc85b3ac79f85a584
SanthoshKumar211299 <subjansan@gmail.com>
2024-07-15 14:00:18 +0530

    new changes related timestamp

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-
-import express from "express";
-import * as dotenv from 'dotenv';
+import express from 'express';
 import fs from 'fs';
+import path from 'path';
 
-dotenv.config();
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-const PORT = process.env.PORT;
+app.use(express.json())
 
-app.get("/", function (request, response) {
-    response.send("Node server is up & running for Node Day1 Task - NodeJS File System!!!");
+app.get('/', (req, res) => {
+    try {
+        const today = new Date().toISOString().replace(/:/g, '-')
+        const filePath = path.join('TimeStamp', `${today}.txt`);
+
+        fs.writeFileSync(filePath, today, 'utf8');
+        const data = fs.readFileSync(filePath, 'utf8');
+        res.status(200).send(data);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal server error');
+    }
 });
 
-//Creating date and time
-const time_stamp = new Date().toString();
-const date_time = new Date();
-const date = ("0" + date_time.getDate()).slice(-2);
-const month = ("0" + (date_time.getMonth() + 1)).slice(-2);
-const year = date_time.getFullYear();
-const hours = date_time.getHours();
-const minutes = date_time.getMinutes();
-const seconds = date_time.getSeconds();
-const currentDateTime = date + "_" + month + "_" + year + "-" + hours + "_" + minutes + "_" + seconds;
+app.get('/getTextFiles', (req, res) => {
+    const folderPath = 'textFile';
 
-// adding new text file to a folder with content current timestamp;
-app.post("/file", function (request, response) {
-    fs.writeFile(`./textFile/${currentDateTime}.txt`, time_stamp, (err) => {
-        console.log("file created!!!");
-        response.send("file created!!!");
-    });
-});
-
-// retrieving the added text files list from folder
-app.get("/file", function (request, response) {
-    fs.readdir("./textFile/", (err, file) => {
+    fs.readdir(folderPath, (err, files) => {
         if (err) {
-            console.log("files not found" + err);
-            response.send("files not found" + err);
+            console.error(err);
+            res.status(500).send('An error occurred while listing the files from the directory');
+        } else {
+            const textFiles = files.filter((file) => path.extname(file) === '.txt');
+            res.status(200).json(textFiles);
         }
-        console.log("Available files:" + file);
-        response.send(file);
     });
-})
+});
 
-app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
