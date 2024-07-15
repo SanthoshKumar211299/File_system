843ad329c07af8f7f06460d188612817503522d5
SanthoshKumar211299 <subjansan@gmail.com>
2024-07-15 13:34:23 +0530

    new added 2 more files

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-import { format } from 'date-fns/format';
-import express from 'express'
-import fs from 'fs'
-import path from 'path';
 
-const PORT=8002;
-let app=express()
-app.use(express.json())
-app.get('/',(req,res)=>{
-    let today=format(new Date(),'dd-MM-yyyy-HH-mm-ss')
-    const filePath = `File_system/${today}.txt`;
-    fs.writeFileSync(filePath,`${today}`,'utf8')
-    let data=fs.readFileSync(filePath,'utf8')
-    try {
-        res.status(200).send(data)
-        
-    } catch (error) {
-        req.res(500).send('Internel server error')
-        
-    }
-})
-//New endpoint to retrieve all text files in a folder
-app.get("/getTextFiles", (req, res) => {
-    const folderPath = "File_system";
-  
-    fs.readdir(folderPath, (err, files) => {
-      if (err) {
-        console.log(err);
-        res
-          .status(500)
-          .send("An error occured while listing the files from directory");
-      } else {
-        const textFiles = files.filter((file) => path.extname(file) === ".txt");
-        res.status(200).json(textFiles);
-      }
+import express from "express";
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", function (request, response) {
+    response.send("Node server is up & running for Node Day1 Task - NodeJS File System!!!");
+});
+
+//Creating date and time
+const time_stamp = new Date().toString();
+const date_time = new Date();
+const date = ("0" + date_time.getDate()).slice(-2);
+const month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+const year = date_time.getFullYear();
+const hours = date_time.getHours();
+const minutes = date_time.getMinutes();
+const seconds = date_time.getSeconds();
+const currentDateTime = date + "_" + month + "_" + year + "-" + hours + "_" + minutes + "_" + seconds;
+
+// adding new text file to a folder with content current timestamp;
+app.post("/file", function (request, response) {
+    fs.writeFile(`./textFile/${currentDateTime}.txt`, time_stamp, (err) => {
+        console.log("file created!!!");
+        response.send("file created!!!");
     });
-  });
+});
+
+// retrieving the added text files list from folder
+app.get("/file", function (request, response) {
+    fs.readdir("./textFile/", (err, file) => {
+        if (err) {
+            console.log("files not found" + err);
+            response.send("files not found" + err);
+        }
+        console.log("Available files:" + file);
+        response.send(file);
+    });
+})
+
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
